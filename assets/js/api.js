836743565421f5b6018fea483f578b82526ea6a5
David William <davidwilliammacedo@icloud.com>
2025-03-26 18:10:25 +0000

    .

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/trydavidqix/json/refs/heads/main/profile.json?token=GHSAT0AAAAAAC5P47NNSJTJW4DUHSJWG5K2Z7EJG3Q';
+    const url = 'https://raw.githubusercontent.com/trydavidqix/desafioPortfolio/refs/heads/main/data/profile.json';
     const response = await fetch(url)
     const profileData = await response.json()
     return profileData
